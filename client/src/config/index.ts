c7430c2bf34ceaf00d2359745126a5ebf505dfc7
harrychanhoyin95 <harrychanhoyin95@gmail.com>
2020-09-06 22:44:42 +0800

    update: client side update api link

--- client/src/config/index.ts
+++ client/src/config/index.ts
@@ -1,2 +1,2 @@
-export const SERVER = 'http://localhost:4000/graphql';
-export const WEB_SOCKET_LINK = 'ws://localhost:4000/graphql';
+export const SERVER = 'https://hacker-news-api-server.herokuapp.com/graphql';
+export const WEB_SOCKET_LINK = 'ws://hacker-news-api-server.herokuapp.com/graphql';
